4834690fe75c14811de2ea0acb5c1578979770a0
Dev. Muhannad Muhammad Gamal <vv3e@outlook.com>
2025-07-01 19:04:13 +0300

    Update train_chatbot.py

--- Python chatbot/train_chatbot.py
+++ Python chatbot/train_chatbot.py
@@ -84,7 +84,7 @@ model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
-
+print("Fuck off") 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
